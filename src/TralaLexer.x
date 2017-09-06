3c73cc4267cc738550fba15f574440d84e110fa5
Maciej Szwaja <tilgalas@gmail.com>
2017-09-06 20:20:01 +0200

    Improve lexer, add support for string literals

--- src/TralaLexer.x
+++ src/TralaLexer.x
@@ -8,22 +8,25 @@ import TralaParsingCommon
 
 $whiteExceptNl = $white # \n
 $digit = 0-9
+$operatorChar = [ \- \+ \* \/ \> \< : = \# @ ]
+$quot = \"
+$stringLitChar = [~$white \ ] # $quot # \\
 
 :-
 
 $whiteExceptNl         ;
 $digit+                { readIntegerLit }
-$digit*\.$digit+       { readFloatLit }
+$digit* \. $digit+     { readFloatLit }
 [_a-zA-Z][_a-zA-Z0-9]* { Id }
 =                      { const EqualSign }
-==                     { const DblEqualSign }
 ::                     { const DblColon }
-[\-\+\*\/\>\<:]+       { Operator }
+$operatorChar+         { Operator }
 \(                     { const LeftParen }
 \)                     { const RightParen }
 true                   { const TrueLit }
 false                  { const FalseLit }
 _                      { const Underscore }
 \.                     { const Dot }
-\\^\n                  ;
+\,                     { const Comma }
 \n                     { const Newline }
+$quot ($stringLitChar|\\$printable)* $quot { readStringLit }
--- src/TralaParsingCommon.hs
+++ src/TralaParsingCommon.hs
@@ -18,7 +18,9 @@ data Token a = EOFToken |
              FalseLit |
              Underscore |
              Newline |
-             Dot
+             Dot |
+             Comma |
+             StringLit a
 
     deriving (Eq, Show)
 
@@ -35,3 +37,38 @@ readFloatLit = FloatLit . (\t -> let fixedT =
                                        if T.head t == '.' then '0' `T.cons` t else t
                                  in
                                    readAnything double fixedT)
+
+windowSearch :: Int -> (Text -> Bool) -> Text -> [Text]
+windowSearch n p t
+  | T.null t = []
+  | otherwise = let (taken, rest) = T.splitAt n t in
+                  if p taken then
+                    taken : windowSearch n p rest
+                  else
+                    taken : windowSearch n p (T.tail t)
+
+unescape2CharSeq :: Text -> Char
+unescape2CharSeq t
+  | (T.null . T.tail) t = T.head t
+  | otherwise = let f = T.head t
+                    s = (T.head . T.tail) t
+                in
+                  if f == '\\' then unescapeChar s else f
+
+unescapeChar :: Char -> Char
+unescapeChar c = case c of
+                   '0' -> '\0'
+                   'a' -> '\a'
+                   'b' -> '\b'
+                   'f' -> '\f'
+                   'n' -> '\n'
+                   'r' -> '\r'
+                   't' -> '\t'
+                   'v' -> '\v'
+                   _ -> c
+
+unescapeChars :: Text -> Text
+unescapeChars t = T.pack $ unescape2CharSeq <$> windowSearch 2 (\t -> T.head t == '\\') t
+
+readStringLit :: Text -> Token Text
+readStringLit = StringLit . unescapeChars . T.init . T.tail
